2d48c0e12b7996d1531858d5bf52e7fc392726bf
nkosakul <nattaphong_kosakul@hotmail.de>
2022-08-26 15:34:01 +0200

    chore: add prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,35 @@
 /* eslint-env node */
-require("@rushstack/eslint-patch/modern-module-resolution");
+require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
-  "root": true,
-  "extends": [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/eslint-config-typescript/recommended"
+  root: true,
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript/recommended',
+    'plugin:vue/vue3-recommended',
+    'prettier',
   ],
-  "parserOptions": {
-    "ecmaVersion": "latest"
-  }
-}
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    'vue/order-in-components': [
+      'error',
+      {
+        order: [
+          'el',
+          'name',
+          ['components', 'directives', 'filters'],
+          'mixins',
+          ['props', 'propsData'],
+          'data',
+          'computed',
+          'methods',
+          'watch',
+          'LIFECYCLE_HOOKS',
+        ],
+      },
+    ],
+  },
+};
