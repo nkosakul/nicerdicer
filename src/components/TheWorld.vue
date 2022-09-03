1ba3976e7f98e8d367062408a678a750c47ac2b5
hanadi92 <hanadi.tamimi@gmail.com>
2022-09-03 18:58:16 +0200

    style: rearrage game board

---
+++ src/components/TheWorld.vue
@@ -0,0 +1,70 @@
+<template>
+  <p>
+    <ThePlayer
+      ref="playerOne"
+      :name="'Player-One'"
+      :is-player-turn="isPlayerOneTurn"
+      :local-player="localPlayer"
+      @played-my-turn="playedMyTurn"
+    />
+  </p>
+  <hr />
+  <p>
+    <ThePlayer
+      ref="playerTwo"
+      :name="'Player-Two'"
+      :is-player-turn="isPlayerTwoTurn"
+      :other-player="otherPlayer"
+      @played-my-turn="playedMyTurn"
+    />
+  </p>
+</template>
+
+<script lang="ts">
+import ThePlayer from '@/components/ThePlayer.vue';
+import { supabase } from '@/supabase';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'TheWorld',
+  components: {
+    ThePlayer,
+  },
+  data() {
+    return {
+      isPlayerOneTurn: true,
+      isPlayerTwoTurn: false,
+      localPlayer: '',
+      otherPlayer: '',
+    };
+  },
+  methods: {
+    playedMyTurn() {
+      if (this.isPlayerOneTurn) {
+        this.isPlayerOneTurn = false;
+        this.isPlayerTwoTurn = true;
+        return;
+      }
+
+      if (this.isPlayerTwoTurn) {
+        this.isPlayerOneTurn = true;
+        this.isPlayerTwoTurn = false;
+        return;
+      }
+    },
+  },
+  created() {
+    const userId = supabase.auth.user()?.id;
+    if (!userId) return;
+    const localSession = JSON.parse(
+      localStorage.getItem('supabase.auth.token') ?? ''
+    );
+    const localSessionUserId = localSession.currentSession?.user?.id || '';
+    if (userId === localSessionUserId) {
+      this.localPlayer = localSessionUserId;
+    }
+
+    // find other users connected to this game.
+  },
+});
+</script>
--- src/views/TheGame.vue
+++ src/views/TheGame.vue
@@ -1,69 +1,19 @@
 <template>
-  <p>
-    <ThePlayer
-      ref="playerOne"
-      :name="'Player-One'"
-      :is-player-turn="isPlayerOneTurn"
-      :local-player="localPlayer"
-      @played-my-turn="playedMyTurn"
-    />
-  </p>
-  <hr />
-  <p>
-    <ThePlayer
-      ref="playerTwo"
-      :name="'Player-Two'"
-      :is-player-turn="isPlayerTwoTurn"
-      :other-player="otherPlayer"
-      @played-my-turn="playedMyTurn"
-    />
-  </p>
+  <TheGame v-if="Object.keys(store.game).length === 0" />
+  <TheWorld v-if="Object.keys(store.game).length !== 0" />
+  <TheLogin v-if="Object.keys(store.game).length !== 0" />
+  <button
+    v-if="Object.keys(store.user).length !== 0"
+    @click="supabase.auth.signOut()"
+  >
+    Logout
+  </button>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
+import { store } from '@/store';
 import { supabase } from '@/supabase';
-import { defineComponent } from 'vue';
-import ThePlayer from '@/components/ThePlayer.vue';
-
-export default defineComponent({
-  name: 'TheWorld',
-  components: {
-    ThePlayer,
-  },
-  data() {
-    return {
-      isPlayerOneTurn: true,
-      isPlayerTwoTurn: false,
-      localPlayer: '',
-      otherPlayer: '',
-    };
-  },
-  methods: {
-    playedMyTurn() {
-      if (this.isPlayerOneTurn) {
-        this.isPlayerOneTurn = false;
-        this.isPlayerTwoTurn = true;
-        return;
-      }
-
-      if (this.isPlayerTwoTurn) {
-        this.isPlayerOneTurn = true;
-        this.isPlayerTwoTurn = false;
-        return;
-      }
-    },
-  },
-  created() {
-    const userId = supabase.auth.user()?.id;
-    const localSession = JSON.parse(
-      localStorage.getItem('supabase.auth.token') || ''
-    );
-    const localSessionUserId = localSession.currentSession?.user?.id || '';
-    if (userId === localSessionUserId) {
-      this.localPlayer = localSessionUserId;
-    }
-
-    // find other users connected to this game.
-  },
-});
+import TheGame from '../components/TheGame.vue';
+import TheLogin from '../components/TheLogin.vue';
+import TheWorld from '../components/TheWorld.vue';
 </script>
